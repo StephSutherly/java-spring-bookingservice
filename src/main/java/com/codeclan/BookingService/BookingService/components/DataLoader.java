0ddff6813a3e4726cf9d79d24b7f527c8f579737
Steph Scully <stephanielarascullion@yahoo.com>
2019-08-08 11:46:46 +0100

    Added @Component to DataLoader to fix bug

--- src/main/java/com/codeclan/BookingService/BookingService/components/DataLoader.java
+++ src/main/java/com/codeclan/BookingService/BookingService/components/DataLoader.java
@@ -9,7 +9,9 @@ import com.codeclan.BookingService.BookingService.respositories.CustomerReposito
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
