65b12097845963d56f64401ccab1027e144ae5ee
Steph Scully <stephanielarascullion@yahoo.com>
2019-08-08 11:41:55 +0100

    Added annotations to bookking model

--- src/main/java/com/codeclan/BookingService/BookingService/models/Booking.java
+++ src/main/java/com/codeclan/BookingService/BookingService/models/Booking.java
@@ -1,10 +1,24 @@
 package com.codeclan.BookingService.BookingService.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "date")
     private String date;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
     private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
 
     public Booking(String date, Course course, Customer customer) {
